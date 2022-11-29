984e1bc8b9027984f9b3a64f904b7dec1c2d7d51
Stalin García <sjgarcia@espol.edu.ec>
2022-11-29 00:18:07 -0500

    elaboracion del codigo para el funcionamiento del buscador

--- Proyecto02/js/personajes.js
+++ Proyecto02/js/personajes.js
@@ -6,9 +6,9 @@ const cargarPersonajes = () => {
         /*const container = document.getElementById("container_characters");*/
         arrPersonajes.forEach((personaje) => {
             document.getElementById("container_characters").innerHTML += `
-                <div class="card">
+                <div id="card" class="card">
                     <img src="${personaje['image']}" class="card-img-top" alt="...">
-                    <div class="card-body">
+                    <div id="card-body" class="card-body">
                         <h5 class="card-title">${personaje.name}</h5>
                     </div>
                 </div>  
@@ -18,8 +18,25 @@ const cargarPersonajes = () => {
     .catch((error) => console.log(error));
 }
 
+const filtrarPersonajes =  () => {
+    document.getElementById('buscador').addEventListener('keyup', (e) => {
+        if(e.target.matches('#buscador')){
+            document.querySelectorAll("#card").forEach((character) => {
+                character.querySelector("#card-body > h5").textContent.toLowerCase().includes(e.target.value)
+                ? character.classList.remove("filter")
+                : character.classList.add("filter");
+            })
+
+        }
+    }
+    
+    )
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
     cargarPersonajes();
+    filtrarPersonajes();
 });
 
 
